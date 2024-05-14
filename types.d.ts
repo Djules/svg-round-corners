5cde5c50f497afbe2e5ac283f26aa5eb3cd86c31
Julien Vasseur <jvasseur87@gmail.com>
2024-05-14 17:02:14 +0200

    Support different radius per corner

--- types.d.ts
+++ types.d.ts
@@ -17,18 +17,18 @@ export function parsePath(str: string): SVGCommand[]
  * Iterates through an array of normalised commands and insert arcs where applicable.
  * This function modifies the array in place.
  * @param {array} _cmds Array with commands to be modified
- * @param {number} r Expected radius of the arcs.
+ * @param {number | number[]} r Expected radius of the arcs.
  * @param {number} round Number of decimal digits to round values
  * @returns {array} Sequence of commands containing arcs in place of corners
  */
-export function roundCommands(cmds: SVGCommand[], r: number, round?: number): { path: string; commands: SVGCommand[] }
+export function roundCommands(cmds: SVGCommand[], r: number | number[], round?: number): { path: string; commands: SVGCommand[] }
 
 /**
  * This is a shorthand for parsePath() and roundCommands().
  * You get the end result in one function call.
  * @param {string} str Raw string with commands from the path element
- * @param {number} r Expected radius of the arcs.
+ * @param {number | number[]} r Expected radius of the arcs.
  * @param {number} round Number of decimal digits to round values
  * @returns {array} New commands sequence with rounded corners
  */
-export function roundCorners(str: string, r: number, round?: number): { path: string; commands: SVGCommand[] }
+export function roundCorners(str: string, r: number | number[], round?: number): { path: string; commands: SVGCommand[] }
